6448a6e0d7375352153859260191196652c9610b
k0va9 <179985748+k0va9@users.noreply.github.com>
2024-11-03 19:12:38 +0900

    feat: implement walk target directory method

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"os"
+	"strings"
 )
 
 var allFlag bool
@@ -13,6 +15,31 @@ func init() {
 	flag.BoolVar(&allFlag, "a", false, "")
 	flag.BoolVar(&longFlag, "l", false, "")
 	flag.BoolVar(&helpFlag, "help", false, "")
+}
+
+type FileInfo struct {
+	name string
+}
+
+func walk(target string, allFlag bool) []FileInfo {
+	var result []FileInfo
+
+	files, _ := os.ReadDir(target)
+
+	for _, f := range files {
+		ent, _ := f.Info()
+		if !allFlag && strings.HasPrefix(ent.Name(), ".") {
+			continue
+		}
+
+		info := FileInfo{
+			name: ent.Name(),
+		}
+
+		result = append(result, info)
+	}
+
+	return result
 }
 
 func main() {
